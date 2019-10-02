02c8125306fa34b21d076da2dd6a1159dff2a17e
skysharkk <sky.light.blr@gmail.com>
2019-10-03 01:37:25 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let strToArray = str.split('');
+    let stack = [];
+    let openBrackets = [];
+    let closeBrackets = [];
+
+    bracketsConfig.forEach(element => {
+        if (element[0]) {
+            openBrackets.push(element[0]);
+        }
+        if (element[1]) {
+            closeBrackets.push(element[1]);
+        }
+    });
+
+    for (let i = 0; i < strToArray.length; i++) {
+        let infexOfOpen = openBrackets.indexOf(strToArray[i]);
+        let infexOfClose = closeBrackets.indexOf(strToArray[i]);
+
+        if (infexOfOpen === infexOfClose) {
+
+            if (stack[stack.length - 1] === infexOfOpen & stack.length > 0) {
+                stack.pop();
+            } else {
+                stack.push(infexOfOpen);
+            }
+        }
+
+        if (infexOfOpen != -1) {
+            stack.push(infexOfOpen);
+        }
+
+        if (infexOfClose != -1) {
+            indexOfOpen = stack.pop();
+
+            if (indexOfOpen != infexOfClose) {
+                return false;
+            }
+        }
+    }
+
+    if (stack.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+
+
+
